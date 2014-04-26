28991fe49b7ee10ea1c4733bd3f2d3329bd91f5e
XxChxppellxX <jack.chappell13@gmail.com>
2014-04-26 22:40:03 +0100

    Added kick event.

--- src/com/lekohd/blockparty/listeners/DisconnectListener.java
+++ src/com/lekohd/blockparty/listeners/DisconnectListener.java
@@ -2,6 +2,7 @@ package com.lekohd.blockparty.listeners;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.lekohd.blockparty.Main;
@@ -13,7 +14,17 @@ import com.lekohd.blockparty.Main;
 public class DisconnectListener implements Listener {
 
 	@EventHandler
-	public void onDisconnect(final PlayerQuitEvent e) {
+	public void onDisconnect(PlayerQuitEvent e) {
+		if(Main.inGamePlayers.containsKey(e.getPlayer()))
+			Main.inGamePlayers.remove(e.getPlayer());
+		if(Main.inLobbyPlayers.containsKey(e.getPlayer()))
+			Main.inLobbyPlayers.remove(e.getPlayer());
+		if(Main.onFloorPlayers.containsKey(e.getPlayer()))
+			Main.onFloorPlayers.remove(e.getPlayer());
+	}
+	
+	@EventHandler
+	public void onKick(PlayerKickEvent e) {
 		if(Main.inGamePlayers.containsKey(e.getPlayer()))
 			Main.inGamePlayers.remove(e.getPlayer());
 		if(Main.inLobbyPlayers.containsKey(e.getPlayer()))
